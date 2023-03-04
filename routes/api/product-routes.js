562bfcd770f101c8a2c85804522dd343794f817f
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-03-03 20:46:33 -0600

    tag routes done

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -10,10 +10,14 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ module: }]
+    })
+  }
+});// find a single product by its `id`
+// be sure to include its associated Category and Tag data
 
 // create new product
 router.post('/', (req, res) => {
@@ -89,8 +93,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
-});
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!'});
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}); // delete one product by its `id` value
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { afterSync } = require('../../config/connection');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
@@ -48,12 +49,23 @@ router.post('/', async (req, res) => {
   }
 }); // create a new tag
 
-router.put('/:id', (req, res) => {
-  Tag.update(
-    {
-      
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(400).json({ message: 'Try again'});
+      return;
     }
-  )
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }); // update a tag's name by its `id` value
 
 router.delete('/:id', async (req, res) => {
